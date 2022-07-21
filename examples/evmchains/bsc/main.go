b1322f599b9c1505e963b2b717fec1beb0924c9a
chenzhijie <chenzhijie@onchain.com>
2022-07-21 13:42:39 +0800

    Add evm chains transfer examples

---
+++ examples/evmchains/bsc/main.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"encoding/hex"
+	"fmt"
+	"math/big"
+	"os"
+
+	"github.com/chenzhijie/go-web3"
+	"github.com/ethereum/go-ethereum/crypto"
+)
+
+func main() {
+
+	// change to your rpc provider
+	var chainId = int64(97)
+	var rpcProvider = "https://data-seed-prebsc-1-s1.binance.org:8545"
+	web3, err := web3.NewWeb3(rpcProvider)
+	if err != nil {
+		panic(err)
+	}
+	web3.Eth.SetChainId(chainId)
+	blockNumber, err := web3.Eth.GetBlockNumber()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Current block number: ", blockNumber)
+
+	// only for test
+	privateKey := os.Getenv("testPrivateKey") // hex string format
+	if len(privateKey) == 0 {
+		panic("please replace to your privateKey and keep safe by yourself")
+	}
+	// setup your privateKey
+	if err := web3.Eth.SetAccount(privateKey); err != nil {
+		panic(err)
+	}
+	privateKeyData, err := hex.DecodeString(privateKey)
+	if err != nil {
+		panic(err)
+	}
+	ecdsaPrivateKey, err := crypto.ToECDSA(privateKeyData)
+	if err != nil {
+		panic(err)
+	}
+
+	addr := crypto.PubkeyToAddress(ecdsaPrivateKey.PublicKey)
+	fmt.Printf("Address %s\n", addr)
+
+	bnbBalance, err := web3.Eth.GetBalance(addr, nil)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Bnb balance %v\n", bnbBalance)
+
+	gasPrice, err := web3.Eth.GasPrice()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("GasPrice %v\n", gasPrice)
+
+	tx, err := web3.Eth.SendRawTransaction(
+		addr,
+		web3.Utils.ToWei(0.1),
+		21000,
+		big.NewInt(int64(gasPrice)),
+		nil,
+	)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Send 0.1 BNB to self tx %s\n", tx)
+}
---
+++ examples/evmchains/polygon/main.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"encoding/hex"
+	"fmt"
+	"os"
+
+	"github.com/chenzhijie/go-web3"
+	"github.com/ethereum/go-ethereum/crypto"
+)
+
+func main() {
+
+	// change to your rpc provider
+	var chainId = int64(80001)
+	var rpcProvider = "https://matic-testnet-archive-rpc.bwarelabs.com"
+	web3, err := web3.NewWeb3(rpcProvider)
+	if err != nil {
+		panic(err)
+	}
+	web3.Eth.SetChainId(chainId)
+	blockNumber, err := web3.Eth.GetBlockNumber()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Current block number: ", blockNumber)
+
+	// only for test
+	privateKey := os.Getenv("testPrivateKey") // hex string format
+	if len(privateKey) == 0 {
+		panic("please replace to your privateKey and keep safe by yourself")
+	}
+	// setup your privateKey
+	if err := web3.Eth.SetAccount(privateKey); err != nil {
+		panic(err)
+	}
+	privateKeyData, err := hex.DecodeString(privateKey)
+	if err != nil {
+		panic(err)
+	}
+	ecdsaPrivateKey, err := crypto.ToECDSA(privateKeyData)
+	if err != nil {
+		panic(err)
+	}
+
+	addr := crypto.PubkeyToAddress(ecdsaPrivateKey.PublicKey)
+	fmt.Printf("Address %s\n", addr)
+
+	maticBalance, err := web3.Eth.GetBalance(addr, nil)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("MATIC balance %v\n", maticBalance)
+
+	tx, err := web3.Eth.SyncSendEIP1559RawTransaction(
+		addr,
+		web3.Utils.ToWei(0.1),
+		21000,
+		web3.Utils.ToGWei(25),
+		web3.Utils.ToGWei(325),
+		nil,
+	)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Send 0.1 MATIC to self tx %s\n", tx.TxHash)
+}
